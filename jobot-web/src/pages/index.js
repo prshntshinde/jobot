044284be0f101bb530d1cc5b840911526291992d
Prashant Shinde <mr.pshinde@gmail.com>
2023-04-17 11:37:15 +0000

    feat: Navbar and API Key - add input box to enter API Key. - Add Navbar.

--- jobot-web/src/pages/index.js
+++ jobot-web/src/pages/index.js
@@ -1,3 +1,24 @@
+import { useState } from "react";
+
 export default function Home() {
-    return <div>Hello World</div>;
+    const [apiKey, setApiKey] = useState("");
+    return (<div className="flex flex-col h-screen">
+        {/* Navbar */}
+        <nav className="bg-white shadow w-full">
+            <div className="px-4 g-14 flex justify-between items-center">
+                <div className="text-xl font-bold">Jobot</div>
+                <div>
+                    <input
+                        type="password"
+                        className="border rounded p-1"
+                        placeholder="Enter API Key..."
+                        value={apiKey}
+                        onChange={(e) => setApiKey(e.target.value)}
+                    />
+                </div>
+            </div>
+        </nav>
+        {/* Add more UI here */}
+    </div>
+    );
 }
