eb4ebc13d0327161725e0362c4fd2e0afaf7095c
Prashant Shinde <mr.pshinde@gmail.com>
2023-04-17 12:00:45 +0000

    feat: API Invocation with Fetch - add button to invoke the chat completions API and show response.

--- jobot-web/src/pages/index.js
+++ jobot-web/src/pages/index.js
@@ -2,6 +2,38 @@ import { useState } from "react";
 
 export default function Home() {
     const [apiKey, setApiKey] = useState("");
+
+    /* API Invocation with Fetch */
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const [botMessage, setBotMessage] = useState("");
+    const sendRequest = async () => {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${apiKey}`,
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: "Hello!" }],
+            }),
+        });
+
+        const resJson = await response.json();
+        console.log(resJson);
+
+        setBotMessage(resJson.choices[0].message.content);
+    };
+
+    /* Button to Invoke the above code */
+    <div className="p-4">
+        <button onClick={sendRequest} className="w-40 bordered rounded bg-blue-500 hover:bg-blue-600 text-white p-2">Send Request </button>
+        <div className="mt-4 text-lg">{botMessage}</div>
+    </div>
+
+
+    /* Add more logic here*/
+
     return (<div className="flex flex-col h-screen">
         {/* Navbar */}
         <nav className="bg-white shadow w-full">
